09a67c1440f07052c4d1f7bdb80f3baaed7a54dc
Chris J. Karr <chris@audacious-software.com>
2025-07-10 15:42:29 -0500

    Updated exporter

--- simple_data_export_api.py
+++ simple_data_export_api.py
@@ -2,6 +2,7 @@
 
 import importlib
 import io
+import hashlib
 import os
 import tempfile
 
@@ -9,6 +10,7 @@ import pytz
 
 from django.conf import settings
 from django.db.models import Q
+from django.utils import timezone
 
 from simple_data_export.utils import fetch_export_identifier, UnicodeWriter # pylint: disable=import-error
 
@@ -40,7 +42,11 @@ def compile_data_export(data_type, data_sources, start_time=None, end_time=None,
     if data_type == 'simple_messaging_dialog_support.dialog_variables':
         filename = tempfile.gettempdir() + os.path.sep + 'simple_messaging_dialog_support.dialog_variables.txt'
 
+        print('simple_messaging_dialog_support.dialog_variables: 1')
+
         with io.open(filename, 'wb') as outfile:
+            print('simple_messaging_dialog_support.dialog_variables: 1.1')
+
             writer = UnicodeWriter(outfile, delimiter='\t')
 
             variables = [
@@ -52,6 +58,8 @@ def compile_data_export(data_type, data_sources, start_time=None, end_time=None,
             ]
 
             dialog_variables = []
+
+            print('simple_messaging_dialog_support.dialog_variables: 1.2')
 
             for app in settings.INSTALLED_APPS:
                 try:
@@ -66,20 +74,30 @@ def compile_data_export(data_type, data_sources, start_time=None, end_time=None,
                 except AttributeError:
                     pass
 
+            print('simple_messaging_dialog_support.dialog_variables: 1.3')
+
             if len(dialog_variables) == 0: # pylint: disable=len-as-condition
-                for variable in DialogVariable.objects.all().order_by('date_set'):
-                    if (variable.key in dialog_variables) is False:
-                        dialog_variables.append(variable.key)
+                for variable_key in DialogVariable.objects.order_by().values_list('key', flat=True).distinct():
+                    if (variable_key in dialog_variables) is False:
+                        dialog_variables.append(variable_key)
 
             variables.extend(dialog_variables)
 
+            print('simple_messaging_dialog_support.dialog_variables: 1.4')
+
             writer.writerow(variables)
+
+            print('simple_messaging_dialog_support.dialog_variables: 1.5')
 
             for session in DialogSession.objects.exclude(finished=None).order_by('started'): # pylint: disable=too-many-nested-blocks
                 destination = session.current_destination()
 
                 if destination in data_sources:
+                    print('simple_messaging_dialog_support.dialog_variables[%s]: 1.5.1' % destination)
+
                     if session.dialog is not None:
+                        print('simple_messaging_dialog_support.dialog_variables[%s]: 1.5.1.1' % destination)
+
                         session_variables = {
                             'Destination': fetch_export_identifier(destination),
                             'Dialog': session.dialog.key,
@@ -91,12 +109,52 @@ def compile_data_export(data_type, data_sources, start_time=None, end_time=None,
                         if session.dialog.finish_reason in ('user_cancelled', 'dialog_cancelled', 'timed_out',):
                             session_variables['Cancelled'] = True
 
-                        for variable in DialogVariable.objects.filter(date_set__gte=session.started, date_set__lte=session.finished).order_by('date_set'):
-                            if variable.current_sender() == destination:
+                        print('simple_messaging_dialog_support.dialog_variables[%s]: 1.5.1.2' % destination)
+
+                        query = Q(lookup_hash=None)
+
+                        hash_obj = hashlib.sha256()
+                        hash_obj.update(destination.encode('utf-8'))
+
+                        hash_lookup = hash_obj.hexdigest()
+
+                        query = query | Q(lookup_hash=hash_lookup)
+
+                        query = query & Q(date_set__gte=session.started)
+                        query = query & Q(date_set__lte=session.finished)
+
+                        print('simple_messaging_dialog_support.dialog_variables[%s]: 1.5.1.3' % destination)
+
+                        variable_pks = DialogVariable.objects.filter(query).order_by('date_set').values_list('pk', flat=True)
+
+                        index = 0
+
+                        print('simple_messaging_dialog_support.dialog_variables[%s]: 1.5.1.4 - %s' % (destination, len(variable_pks)))
+
+                        for variable_pk in variable_pks: # in DialogVariable.objects.order_by('date_set'):
+                            if (index % 500) == 0:
+                                print('simple_messaging_dialog_support.dialog_variables[%s]: %s / %s -- %s' % (destination, index, len(variable_pks), timezone.now().isoformat()))
+
+                            index += 1
+
+                            variable = DialogVariable.objects.get(pk=variable_pk)
+
+                            sender = variable.current_sender()
+
+                            hash_obj = hashlib.sha256()
+                            hash_obj.update(sender.encode('utf-8'))
+
+                            hash_lookup = hash_obj.hexdigest()
+
+                            if sender == destination:
                                 if (variable.key in session_variables) is False:
                                     session_variables[variable.key] = []
 
                                 session_variables[variable.key].append(variable.value)
+
+                            if variable.lookup_hash is None:
+                                variable.lookup_hash = hash_lookup
+                                variable.save()
 
                         row = []
 
@@ -120,6 +178,8 @@ def compile_data_export(data_type, data_sources, start_time=None, end_time=None,
     if data_type == 'simple_messaging_dialog_support.dialog_variable_timeline':
         filename = tempfile.gettempdir() + os.path.sep + 'simple_messaging_dialog_support.dialog_variables.txt'
 
+        print('dialog_variable_timeline 1.1')
+
         with io.open(filename, 'wb') as outfile:
             writer = UnicodeWriter(outfile, delimiter='\t')
 
@@ -131,18 +191,65 @@ def compile_data_export(data_type, data_sources, start_time=None, end_time=None,
                 'Value',
             ]
 
+            print('dialog_variable_timeline 1.1.2')
+
             writer.writerow(columns)
 
-            for variable in DialogVariable.objects.order_by('date_set'):
-                row = []
+            for data_source in data_sources:
+                print('dialog_variable_timeline 1.1.2 -- %s' % data_source)
 
-                row.append(fetch_export_identifier(variable.current_sender()))
-                row.append(variable.dialog_key)
-                row.append(variable.date_set.astimezone(here_tz).isoformat())
-                row.append(variable.key)
-                row.append(variable.value)
+                source_names = [data_source]
 
-                writer.writerow(row)
+                export_name = fetch_export_identifier(data_source)
+
+                if export_name != data_source:
+                    source_names.append(export_name)
+
+                query = Q(lookup_hash=None)
+
+                for source_name in source_names:
+                    hash_obj = hashlib.sha256()
+                    hash_obj.update(source_name.encode('utf-8'))
+
+                    hash_lookup = hash_obj.hexdigest()
+
+                    query = query | Q(lookup_hash=hash_lookup)
+
+                variable_pks = DialogVariable.objects.filter(query).order_by('date_set').values_list('pk', flat=True)
+
+                print('dialog_variable_timeline 1.1.3 %s' % len(variable_pks))
+
+                index = 0
+
+                for variable_pk in variable_pks: # in DialogVariable.objects.order_by('date_set'):
+                    if (index % 500) == 0:
+                        print('dialog_variable_timeline: %s / %s -- %s' % (index, len(variable_pks), timezone.now().isoformat()))
+
+                    index += 1
+
+                    variable = DialogVariable.objects.get(pk=variable_pk)
+
+                    sender = variable.current_sender()
+
+                    hash_obj = hashlib.sha256()
+                    hash_obj.update(sender.encode('utf-8'))
+
+                    hash_lookup = hash_obj.hexdigest()
+
+                    if sender in source_names:
+                        row = []
+
+                        row.append(fetch_export_identifier(variable.current_sender()))
+                        row.append(variable.dialog_key)
+                        row.append(variable.date_set.astimezone(here_tz).isoformat())
+                        row.append(variable.key)
+                        row.append(variable.value)
+
+                        writer.writerow(row)
+
+                    if variable.lookup_hash is None:
+                        variable.lookup_hash = hash_lookup
+                        variable.save()
 
         return filename
 
