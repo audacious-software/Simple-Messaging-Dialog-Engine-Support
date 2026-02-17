5f59bc1cbfd03170108285b2f43ad691a4812dec
Chris J. Karr <chris@audacious-software.com>
2026-02-17 17:13:15 -0600

    Launch keyword updates

--- management/commands/nudge_active_sessions.py
+++ management/commands/nudge_active_sessions.py
@@ -1,8 +1,10 @@
 # pylint: disable=no-member, line-too-long
 
+import importlib
 import logging
 import traceback
 
+from django.conf import settings
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
 
@@ -24,15 +26,43 @@ class Command(BaseCommand):
         exception = None
 
         for session in DialogSession.objects.filter(finished=None).order_by('-pk'):
-            logging.info('Nudging session: %s', session)
+            do_nudge = True
 
-            try:
-                session.process_response(None, None, send_messages=False, logger=logging.getLogger())
-            except Exception as exc: # pylint: disable=bare-except, broad-exception-caught
-                logging.error('Error encountered with session %s:', session.pk)
-                logging.error(traceback.format_exc())
+            session_variables = session.fetch_latest_variables()
 
-                exception = exc
+            for app in settings.INSTALLED_APPS:
+                if do_nudge:
+                    try:
+                        dialog_module = importlib.import_module('.dialog_api', package=app)
+
+                        do_nudge = dialog_module.allow_session_nudge(session)
+                    except ImportError:
+                        pass # traceback.print_exc()
+                    except AttributeError:
+                        pass # traceback.print_exc()
+
+            launch_keyword = session_variables.get('simple_messaging_launch_keyword', None)
+            launch_keyword_consumed = session_variables.get('simple_messaging_launch_keyword_consumed', False)
+
+            if launch_keyword is not None and launch_keyword_consumed is False:
+                do_nudge = True
+
+                session.add_variable('simple_messaging_launch_keyword_consumed', True)
+            else:
+                launch_keyword = None
+
+            if do_nudge:
+                logging.info('Nudging session: %s', session)
+
+                try:
+                    session.process_response(launch_keyword, None, send_messages=False, logger=logging.getLogger())
+                except Exception as exc: # pylint: disable=bare-except, broad-exception-caught
+                    logging.error('Error encountered with session %s:', session.pk)
+                    logging.error(traceback.format_exc())
+
+                    exception = exc
+            else:
+                logging.info('Skipping nudge for session:%s.' % session.pk)
 
         logging.info('Open sessions nudged.')
 
--- models.py
+++ models.py
@@ -164,8 +164,10 @@ class DialogSession(models.Model):
                     except: # pylint: disable=bare-except
                         logger.error(traceback.format_exc())
 
-                if isinstance(last_message, DialogVariableWrapper):
-                    last_message = last_message.storage()
+                if isinstance(last_message['value'], DialogVariableWrapper):
+                    last_message = last_message['value'].storage
+
+                print('[last_message]: %s -- %s' % (last_message, type(last_message)))
 
                 variable_value = 'json:%s' % json.dumps(last_message)
 
